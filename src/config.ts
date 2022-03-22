f9cdd0811bd121936411cf78b40f440bd96ce2ce
Tommy Byrd <tcbyrd@github.com>
2022-03-22 18:45:08 -0400

    change something

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-	title: 'Your Documentation (from a fork)',
+	title: 'Update Documentation from a fork',
 	description: 'Your website description.',
 	defaultLanguage: 'en_US',
 };
