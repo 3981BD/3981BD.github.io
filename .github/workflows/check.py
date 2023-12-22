2f158639f072ef32798314c4ef58527a373a3146
Tommy Byrd <tcbyrd@github.com>
2023-12-22 12:58:37 -0500

    use Request instead of gh api
    this allows more control over request/response lifecycle since `subprocess` becomes dependent on how `gh` formats output.

--- .github/workflows/check.py
+++ .github/workflows/check.py
@@ -1,13 +1,37 @@
 import subprocess
 import os
 import json
-from urllib.request import urlopen
-github_sha = os.environ['GITHUB_SHA']
-github_repository = os.environ['GITHUB_REPOSITORY']
+from urllib.request import urlopen, Request
+
+github_sha = os.environ.get('GITHUB_SHA')
+github_repository = os.environ.get('GITHUB_REPOSITORY')
+
+if not github_repository:
+    raise ValueError("Missing GITHUB_REPOSITORY environment variable")
+if not github_sha:
+    raise ValueError("Missing GITHUB_SHA environment variable")
+
 pages_build_version = urlopen('https://subdomain.func.host/pages-build-version').read().decode()
-response = subprocess.getoutput(f'gh api repos/{github_repository}/compare/{pages_build_version}...{github_sha}')
+
+url = f"https://api.github.com/repos/{github_repository}/compare/{pages_build_version}...{github_sha}"
+headers = {
+    "Authorization": f"Bearer {os.environ.get('GH_TOKEN')}"
+}
+
+req = Request(url, headers=headers)
+try:
+    response = urlopen(req).read().decode()
+except Exception as e:
+    print(f"::error::Failed to fetch comparison data: {e}")
+    raise
+
 print(f"::notice::response: {response}")
-comparison = json.loads(response)
-print(f"::notice::comparison['status']: {comparison['status']}")
+data = json.loads(response)
+# check if data contains an error response
+if 'message' in data:
+    print(f"::warning::errors: {data['message']}")
+    raise ValueError(f"GitHub API returned an error: {data['message']}")
+
+print(f"::notice::comparison['status']: {data['status']}")
 print(f"::notice::GITHUB_SHA: {github_sha}")
 print(f"::notice::pages_build_version: {pages_build_version}")
